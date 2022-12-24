50dc5b3a8680cf6c9f9085df2bcf8f9bbd747574
joe <31175268+jostasik@users.noreply.github.com>
2022-12-24 18:33:29 -0500

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-const map = L.map('map',{scrollWheelZoom:false}).setView([-29.50, 145], 3.5);
+const map = L.map('map',{scrollWheelZoom:false}).setView([41.15, -96.50], 5);
 
 const basemaps = { 
   StreetView: L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',   {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}),
@@ -10,37 +10,37 @@ L.control.layers(basemaps).addTo(map);
 
 basemaps.StreetView.addTo(map);
 
-const basicBeachIcon = L.icon({iconUrl: 'https://raw.githubusercontent.com/shacheeswadia/leaflet-map/main/beach-icon-colorful.svg', iconSize: [25, 25], // size of the icon
+const dIcon = L.icon({iconUrl: 'https://raw.githubusercontent.com/jostasik/domiknows/main/docs/c3d68a9df8970596b85806579fe0ff96.svg', iconSize: [25, 25], // size of the icon
 });
 
 
-const marker1 = L.marker([-37.699450, 176.279420], {icon: basicBeachIcon})
+const marker1 = L.marker([-37.699450, 176.279420], {icon: dIcon})
             .bindPopup('Whitehaven Beach, Whitsunday Island')
             .addTo(map);
-const marker2 = L.marker([-27.643310, 153.305140], {icon: basicBeachIcon})
+const marker2 = L.marker([-27.643310, 153.305140], {icon: dIcon})
   .bindPopup('Turquoise Bay Exmouth, Australia')
   .addTo(map);
-const marker3 = L.marker([-33.956330, 122.150270], {icon: basicBeachIcon})
+const marker3 = L.marker([-33.956330, 122.150270], {icon: dIcon})
   .bindPopup('Cape Le Grand National Park Esperance, Australia')
   .addTo(map);
-const marker4 = L.marker([-34.962390, 117.391220], {icon: basicBeachIcon})
+const marker4 = L.marker([-34.962390, 117.391220], {icon: dIcon})
   .bindPopup('Greens Pool Denmark, Australia')
   .addTo(map);
-const marker5 = L.marker([-17.961210, 122.214820], {icon: basicBeachIcon})
+const marker5 = L.marker([-17.961210, 122.214820], {icon: dIcon})
   .bindPopup('Cable Beach Broome, Australia')
   .addTo(map);
-const marker6 = L.marker([-16.505960, 151.751520], {icon: basicBeachIcon})
+const marker6 = L.marker([-16.505960, 151.751520], {icon: dIcon})
   .bindPopup('Matira Beach, Society Islands')
   .addTo(map);
-const marker7 = L.marker([-22.594400, 167.484440], {icon: basicBeachIcon})
+const marker7 = L.marker([-22.594400, 167.484440], {icon: dIcon})
   .bindPopup('Piscine Naturelle Ile Des Pins, New Caledonia')
   .addTo(map);
-const marker8 = L.marker([-37.977000, 177.057000], {icon: basicBeachIcon})
+const marker8 = L.marker([-37.977000, 177.057000], {icon: dIcon})
   .bindPopup('Ohope Beach Whakatane, New Zealand')
   .addTo(map);
-const marker9 = L.marker([-41.037600, 173.017000], {icon: basicBeachIcon})
+const marker9 = L.marker([-41.037600, 173.017000], {icon: dIcon})
   .bindPopup('Kaiteriteri Beach, New Zealand')
   .addTo(map);
-const marker10 = L.marker([-37.670300, 176.212000], {icon: basicBeachIcon})
+const marker10 = L.marker([-37.670300, 176.212000], {icon: dIcon})
   .bindPopup('Mt Maunganui Main Beach, New Zealand')
   .addTo(map);
