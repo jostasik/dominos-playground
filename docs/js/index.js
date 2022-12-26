3c783170b616a96a33ad5cb09b84be97270e6ca5
joe <31175268+jostasik@users.noreply.github.com>
2022-12-26 02:21:09 -0500

    :)

--- docs/js/index.js
+++ docs/js/index.js
@@ -14,12 +14,9 @@ var controlLayers = L.control.layers(null, null, {
     collapsed: false
 }).addTo(map);
 
-var dark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {maxZoom: 20, attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'}).addTo(map);
-
-var light = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'});
+var light = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
 
 controlLayers.addBaseLayer(light, 'Day');
-controlLayers.addBaseLayer(dark, 'Night');
 
 // see more basemap options at https://leaflet-extras.github.io/leaflet-providers/preview/
 
