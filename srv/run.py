589e46a57b8f0f9eb9f424f824dbf9a9c7a28631
joe <31175268+jostasik@users.noreply.github.com>
2023-01-03 08:24:29 -0500

    :)

--- srv/run.py
+++ srv/run.py
@@ -4,23 +4,22 @@ import requests
 import os
 import json
 
-def update_eta(store):
-    r = requests.get('https://order.dominos.com/power/store/'+str(store)+'/profile').json()
+def update_eta(store_info):
+    r = requests.get('https://order.dominos.com/power/store/'+str(store_info[0])+'/profile').json()
     try:
         isOpen = r["IsOpen"]
-        phone = r["Phone"]
-        latitude = r["StoreLocation"]["Latitude"]
-        longitude = r["StoreLocation"]["Longitude"]
-        city = r["City"]
-        state = r["Region"]
         storeTime = r["StoreAsOfTime"]
         deliveryEta = r['EstimatedWaitMinutes']
-        return store, isOpen, phone, latitude, longitude, city, state, storeTime, deliveryEta
+        # Return all of the columns from the input file
+        return store_info[0], store_info[1], store_info[2], store_info[3], store_info[4], store_info[5], store_info[6], isOpen, storeTime, deliveryEta
     except Exception as e:
         print(e)
-        return store, None, None, None
+        # Return all of the columns from the input file, with 'error' values for the additional data
+        return store_info[0], store_info[1], store_info[2], store_info[3], store_info[4], store_info[5], store_info[6], 'error', 'error', 'error'
 
-with open('./srv/persistData.csv', 'r') as csvfile:
+
+
+with open('storeData.csv', 'r') as csvfile:
     # Create a CSV reader object
     reader = csv.reader(csvfile)
 
@@ -28,7 +27,7 @@ with open('./srv/persistData.csv', 'r') as csvfile:
     header = next(reader)
 
     # Create a list of tasks
-    tasks = [(row[0]) for row in reader]
+    tasks = [(row) for row in reader]
 
 # Create a thread pool
 with concurrent.futures.ThreadPoolExecutor() as executor:
@@ -39,17 +38,19 @@ with concurrent.futures.ThreadPoolExecutor() as executor:
     rows = []
 
     # Iterate over the completed tasks and process the results
-    for future in concurrent.futures.as_completed(results):
-        store, isOpen, phone, latitude, longitude, city, state, storeTime, deliveryEta = future.result()
-        rows.append([store, isOpen, phone, latitude, longitude, city, state, storeTime, deliveryEta])
+for future in concurrent.futures.as_completed(results):
+    # Unpack all of the values returned by the update_eta function
+    store, latitude, longitude, city, state, franchisee, phone, isOpen, storeTime, deliveryEta = future.result()
+    rows.append([store, latitude, longitude, city, state, franchisee, phone, isOpen, storeTime, deliveryEta])
+
 
 # Open the file in write mode
-with open('./srv/refreshData.csv', 'w', newline='') as csvfile:
+with open('latestStats.csv', 'w', newline='') as csvfile:
     # Create a CSV writer object
     writer = csv.writer(csvfile)
 
     # Write the header row
-    writer.writerow(['Store', 'IsOpen', 'Phone', 'Latitude', 'Longitude', 'City', 'Region', 'StoreAsOfTime', 'EstimatedWaitMinutes'])
+    writer.writerow(['StoreID', 'Latitude', 'Longitude', 'City', 'State', 'Franchisee', 'Phone', 'IsOpen', 'StoreAsOfTime', 'EstimatedWaitMinutes'])
 
     # Write the updated rows
     for row in rows:
