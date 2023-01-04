55cacfb640df7f64af05372461e054cf5ea1cd3f
joe <31175268+jostasik@users.noreply.github.com>
2023-01-04 09:54:03 -0500

    :)

--- js/index.js
+++ js/index.js
@@ -2,19 +2,19 @@
 var map = L.map('map', {
     center: [40.4140, -74.3520],
     // EDIT latitude, longitude to re-center map
-    zoom: 4.4,
+    zoom: 8,
     // EDIT from 1 to 18 -- decrease to zoom out, increase to zoom in
     scrollWheelZoom: true,
     tap: false
 });
 
-/* Control panel to display map layers */
-var controlLayers = L.control.layers(null, null, {
-    position: "topright",
-    collapsed: false
-}).addTo(map);
+// /* Control panel to display map layers */
+// var controlLayers = L.control.layers(null, null, {
+//     position: "topright",
+//     collapsed: false
+// }).addTo(map);
 
-var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>'}).addTo(map);
+var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png').addTo(map);
 
 //'Store', 'IsOpen', 'Phone', 'City', 'Region', 'StoreAsOfTime', 'EstimatedWaitMinutes'
 
