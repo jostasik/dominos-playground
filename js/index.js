d2293a1d34ca1666dddf6747b64140061c9a5912
joe <31175268+jostasik@users.noreply.github.com>
2023-01-05 09:09:12 -0500

    :)

--- js/index.js
+++ js/index.js
@@ -1,20 +1,28 @@
 // Set up initial map center and zoom level
 var map = L.map('map', {
-    center: [40.4140, -74.3520],
+    center: [39.8283, -98.5795],
     // EDIT latitude, longitude to re-center map
-    zoom: 8,
+    zoom: 5,
     // EDIT from 1 to 18 -- decrease to zoom out, increase to zoom in
     scrollWheelZoom: true,
     tap: false
+
 });
 
-// /* Control panel to display map layers */
-// var controlLayers = L.control.layers(null, null, {
-//     position: "topright",
-//     collapsed: false
-// }).addTo(map);
+/* Control panel to display map layers */
+var controlLayers = L.control.layers(null, null, {
+    position: "topright",
+    collapsed: false
+}).addTo(map);
 
-var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png').addTo(map);
+var dark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_nolabels/{z}/{x}/{y}{r}.png');
+controlLayers.addBaseLayer(dark, 'Dark');
+var light = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', { attribution: '&copy; <a href="http://www.openstreetmap.org/copyright"></a>, &copy; <a href="https://carto.com/attribution"></a>' });
+controlLayers.addBaseLayer(light, 'Light');
+var street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
+controlLayers.addBaseLayer(street, 'Street')
+
+
 
 //'Store', 'IsOpen', 'Phone', 'City', 'Region', 'StoreAsOfTime', 'EstimatedWaitMinutes'
 
