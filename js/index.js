90f1bfd36d79e3e7e453bdf936605b1208712f2c
joe <31175268+jostasik@users.noreply.github.com>
2023-01-08 19:54:09 -0500

    updated popup content

--- js/index.js
+++ js/index.js
@@ -29,7 +29,7 @@ controlLayers.addBaseLayer(satellite, 'Satellite')
 
 var markerClusterGroup = L.markerClusterGroup({
 	maxClusterRadius: 70,
-    disableClusteringAtZoom: 11,
+    // disableClusteringAtZoom: 11,
 	showCoverageOnHover: true,
 	zoomToBoundsOnClick: true,
 
@@ -45,28 +45,42 @@ $.get('srv/latestStats.csv', function(csvString) {
     var markerIcon = L.icon({ iconUrl: 'images/marker.svg', iconSize: [32, 32], iconAnchor: [16, 16] });
 
     for (var i in data) {
-        var row = data[i];
+        var row = data[i]; 
 
+        const url = 'https://dominos-backend.vercel.app/api/profile?storeId=' + row.StoreID;
+                
         // Create a string with the contents of the pop-up window
-        var popupContent = '<b> Domino\'s # ' + row.StoreID + '</b><br>' +
-            '<i> Franchisee: <b>' + row.Franchisee + '</b></i></br>' +
-            '<i> Open: <b>' + row.IsOpen + '</b></i></br>' +
+        const popupContent = '<em>Domino\'s #<b>' + row.StoreID + '</b></em><br>'+
+            '<i> Franchisee: ' + row.Franchisee + '</i></br>' +
             'Phone: ' + row.Phone + '<br>' +
-            'City: ' + row.City + ', ' + row.State + '<br>' +
-            'Estimated Wait Minutes: <b>' + row.EstimatedWaitMinutes + '</b><br>' +
-            '(<i>as of: ' + row.StoreAsOfTime + ')</i>';
+            'City: ' + row.City + ', ' + row.State;
 
         // Create a marker and bind the pop-up window to it
-        var marker = L.marker([row.Latitude, row.Longitude], {
-            opacity: 1,
-            icon: markerIcon
-        }).bindPopup(popupContent);
+        var marker = L.marker([row.Latitude, row.Longitude], {opacity: 1, icon: markerIcon}).bindPopup(popupContent);
 
         markerClusterGroup.addLayer(marker);
-    }
 
 
-});
+        marker.on('click', function(e) {
+            var popup = e.target.getPopup();
+            $.getJSON(url).done(function(data) {
+                var liveDetails = '<hr>' +
+                    'Open: <span style="text-transform:capitalize"><b>' + data.IsOpen + '</b></span><br>' +
+                    'Deliveries: <b>' + data.ServiceMethodEstimatedWaitMinutes.Delivery.Min + '-' + data.ServiceMethodEstimatedWaitMinutes.Delivery.Max + '</b> mins<sup>1</sup><br>' +
+                    'Carryouts: <b>' + data.ServiceMethodEstimatedWaitMinutes.Carryout.Min + '-' + data.ServiceMethodEstimatedWaitMinutes.Carryout.Max + '</b> mins<sup>1</sup><br>' +
+                    '<sub><sup>[1]</sup><i>as of: ' + data.StoreAsOfTime + '</i></sub>';
+                popup.setContent(popupContent + '<br>' + liveDetails);
+                popup.update();
+            })
+        })
+    };
+})
+    
+
+
+
+    
+
 
 map.attributionControl.setPrefix(
     'developed by <a href="https://github.com/jostasik/dominos" target="_blank">@jostasik</a>'
