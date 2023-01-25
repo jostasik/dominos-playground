a5d47f5eba0b45f34a0b3c01b95daadec90de3b8
joe <31175268+jostasik@users.noreply.github.com>
2023-01-25 10:36:07 -0500

    :)

--- js/map.js
+++ js/map.js
@@ -1,51 +1,60 @@
-var map = L.map("map", {
-  center: [37, -96],
-  zoom: 5,
-  minZoom: 4,
-  maxZoom: 14,
-  zoomSnap: 0.5,
-  scrollWheelZoom: true,
-  tap: false,
-  maxBounds: [
-    [2.826395, -151.609901],
-    [61.108057, -39.461464],
-  ],
-})
-
-var markerIcon = L.icon({ iconUrl: "images/marker.svg", iconSize: [32, 32], iconAnchor: [16, 16] })
-
-var controlLayers = L.control.layers(null, null, { position: "topright" }).addTo(map)
-
-var street = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> map tiles',
-}).addTo(map)
-controlLayers.addBaseLayer(street, "Street")
-
-var dark = L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-  attribution: '&copy; <a href="https://carto.com/attribution">Carto</a> map tiles',
-})
-controlLayers.addBaseLayer(dark, "Dark")
-
-var light = L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-  attribution: '&copy; <a href="https://carto.com/attribution">Carto</a> map tiles',
-})
-controlLayers.addBaseLayer(light, "Light")
-
-L.control
-  .locate({
-    position: "topleft",
-    locateOptions: {
-      flyTo: true,
-      maxZoom: 12,
-    },
-  })
-  .addTo(map)
-
-var markers = L.markerClusterGroup({
-  maxClusterRadius: 90,
-  showCoverageOnHover: true,
-  zoomToBoundsOnClick: true,
-  chunkedLoading: true,
-}).addTo(map)
-
+const map = L.map("map", { center: [37, -96], zoom: 4, minZoom: 4, maxZoom: 16 })
 map.attributionControl.setPrefix('developed by <a href="https://jostasik.com" target="_blank">Joe Stasik</a>')
+
+const markerIcon = L.icon({ iconUrl: "images/marker.svg", iconSize: [32, 32], iconAnchor: [16, 16] })
+
+const dark = L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+  attribution: '&copy; <a href="https://carto.com/attribution">Carto</a>',
+})
+
+const light = L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
+  attribution: '&copy; <a href="https://carto.com/attribution">Carto</a>',
+})
+
+const street = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution: '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map)
+
+const controlLayers = L.control.layers({ Street: street, Dark: dark, Light: light }).addTo(map)
+
+const url = "https://api.jostasik.com/api/profile?storeId="
+
+const template = `Domino's <b>{{storeId}}</b><br>
+                  Franchisee: {{franchisee}}<br>
+                  <span style='right'><i>City: {{city}}, {{region}}<br>
+                  Phone: {{phone}}<hr>
+                  Open: <span style='text-transform:capitalize'><b>{{isOpen}}</b><br>
+                  Deliveries: <b>{{deliveryMin}}-{{deliveryMax}}</b> mins<br>
+                  Carryouts: <b>{{carryoutMin}}-{{carryoutMax}}</b> mins<br>
+                  <sub><i>as of: {{storeAsOfTime}}</i></sub><br>`
+
+L.control.locate({ position: "topleft", locateOptions: { flyTo: true, maxZoom: 12 } }).addTo(map)
+
+$.getJSON("data/info.json", function (data) {
+  data.forEach(function (row) {
+    let marker = L.marker([row.y, row.x], { opacity: 1, icon: markerIcon }).bindPopup("Loading...")
+    markers.addLayer(marker)
+    marker.on("click", function (e) {
+      let popup = e.target.getPopup()
+      $.getJSON(url + row.i).done(function (data) {
+        let liveDetails = template
+          .replace("{{storeId}}", row.i)
+          .replace("{{franchisee}}", row.f)
+          .replace("{{city}}", data.City)
+          .replace("{{region}}", data.Region)
+          .replace("{{phone}}", data.Phone)
+          .replace("{{isOpen}}", data.IsOpen)
+          .replace("{{deliveryMin}}", data.ServiceMethodEstimatedWaitMinutes.Delivery.Min)
+          .replace("{{deliveryMax}}", data.ServiceMethodEstimatedWaitMinutes.Delivery.Max)
+          .replace("{{carryoutMin}}", data.ServiceMethodEstimatedWaitMinutes.Carryout.Min)
+          .replace("{{carryoutMax}}", data.ServiceMethodEstimatedWaitMinutes.Carryout.Max)
+          .replace("{{storeAsOfTime}}", data.StoreAsOfTime)
+
+        popup.setContent(liveDetails)
+        popup.update()
+      })
+    })
+  })
+})
+
+const markers = L.markerClusterGroup({ maxClusterRadius: 100 }).addTo(map)
