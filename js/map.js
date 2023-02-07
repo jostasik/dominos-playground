c1f08c9599c8a03ecee3c525ecb7e6e7c55ef6d4
joe <31175268+jostasik@users.noreply.github.com>
2023-02-07 12:58:46 -0500

    :)

--- js/map.js
+++ js/map.js
@@ -3,58 +3,68 @@ map.attributionControl.setPrefix('developed by <a href="https://jostasik.com" ta
 
 const markerIcon = L.icon({ iconUrl: "images/marker.svg", iconSize: [32, 32], iconAnchor: [16, 16] })
 
-const dark = L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
-  attribution: '&copy; <a href="https://carto.com/attribution">Carto</a>',
-})
-
-const light = L.tileLayer("https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png", {
-  attribution: '&copy; <a href="https://carto.com/attribution">Carto</a>',
-})
-
 const street = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: '&copy;<a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map)
 
-const controlLayers = L.control.layers({ Street: street, Dark: dark, Light: light }).addTo(map)
+const satellite = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}")
 
-const url = "https://api.jostasik.com/api/profile?storeId="
+const dark = L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {
+  attribution: '&copy; <a href="https://carto.com/attribution">Carto</a>',
+})
 
-const template = `Domino's <b>{{storeId}}</b><br>
-                  Franchisee: {{franchisee}}<br>
-                  <span style='right'><i>City: {{city}}, {{region}}<br>
-                  Phone: {{phone}}<hr>
-                  Open: <span style='text-transform:capitalize'><b>{{isOpen}}</b><br>
-                  Deliveries: <b>{{deliveryMin}}-{{deliveryMax}}</b> mins<br>
-                  Carryouts: <b>{{carryoutMin}}-{{carryoutMax}}</b> mins<br>
-                  <sub><i>as of: {{storeAsOfTime}}</i></sub><br>`
+const controlLayers = L.control.layers({ Street: street, Satellite: satellite, Dark: dark }).addTo(map)
 
 L.control.locate({ position: "topleft", locateOptions: { flyTo: true, maxZoom: 12 } }).addTo(map)
 
-$.getJSON("data/info.json", function (data) {
-  data.forEach(function (row) {
-    let marker = L.marker([row.y, row.x], { opacity: 1, icon: markerIcon }).bindPopup("Loading...")
-    markers.addLayer(marker)
-    marker.on("click", function (e) {
-      let popup = e.target.getPopup()
-      $.getJSON(url + row.i).done(function (data) {
-        let liveDetails = template
-          .replace("{{storeId}}", row.i)
-          .replace("{{franchisee}}", row.f)
-          .replace("{{city}}", data.City)
-          .replace("{{region}}", data.Region)
-          .replace("{{phone}}", data.Phone)
-          .replace("{{isOpen}}", data.IsOpen)
-          .replace("{{deliveryMin}}", data.ServiceMethodEstimatedWaitMinutes.Delivery.Min)
-          .replace("{{deliveryMax}}", data.ServiceMethodEstimatedWaitMinutes.Delivery.Max)
-          .replace("{{carryoutMin}}", data.ServiceMethodEstimatedWaitMinutes.Carryout.Min)
-          .replace("{{carryoutMax}}", data.ServiceMethodEstimatedWaitMinutes.Carryout.Max)
-          .replace("{{storeAsOfTime}}", data.StoreAsOfTime)
+$.get("data/stores.csv", function (csvString) {
+  var data = Papa.parse(csvString, { header: true, dynamicTyping: true }).data
 
-        popup.setContent(liveDetails)
+  for (var i in data) {
+    var row = data[i]
+
+    const url = "https://api.jostasik.com/api/profile?storeId=" + row.StoreID
+
+    const popupContent = "<b>Domino's #" + row.StoreID + "<br>" + "Franchisee: " + row.Franchisee + "</b>"
+
+    let marker = L.marker([row.Latitude, row.Longitude], { opacity: 1, icon: markerIcon }).bindPopup("Loading...")
+
+    markerGroup.addLayer(marker)
+
+    marker.on("click", function (e) {
+      var popup = e.target.getPopup()
+      $.getJSON(url).done(function (data) {
+        const liveDetails =
+          "<span style='right'><i>City: " +
+          data.City +
+          ", " +
+          data.Region +
+          "<br>" +
+          "Phone: " +
+          data.Phone +
+          "<hr>" +
+          "Open: <span style='text-transform:capitalize'><b>" +
+          data.IsOpen +
+          "</b><sup>1</sup><br>" +
+          "Deliveries: <b>" +
+          data.ServiceMethodEstimatedWaitMinutes.Delivery.Min +
+          "-" +
+          data.ServiceMethodEstimatedWaitMinutes.Delivery.Max +
+          "</b> mins<sup>1</sup><br>" +
+          "Carryouts: <b>" +
+          data.ServiceMethodEstimatedWaitMinutes.Carryout.Min +
+          "-" +
+          data.ServiceMethodEstimatedWaitMinutes.Carryout.Max +
+          "</b> mins<sup>1</sup><br>" +
+          "<sub><sup>[1]</sup><i>as of: " +
+          data.StoreAsOfTime +
+          "</i></sub>"
+
+        popup.setContent(popupContent + "<br>" + liveDetails)
         popup.update()
       })
     })
-  })
+  }
 })
 
-const markers = L.markerClusterGroup({ maxClusterRadius: 100 }).addTo(map)
+const markerGroup = L.markerClusterGroup({ maxClusterRadius: 60 }).addTo(map)
