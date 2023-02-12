06916a3d2fbd7052077daa752f730d4a070d7bf8
joe <31175268+jostasik@users.noreply.github.com>
2023-02-12 10:51:40 -0500

    lets goooooo

--- js/map.js
+++ js/map.js
@@ -1,18 +1,23 @@
-var map = L.map("map", { center: [40.5, -77.5], zoom: 7, minZoom: 4, maxZoom: 16, preferCanvas: true, worldCopyJump: true })
+var map = L.map("map", {center: [40.5, -86.5], zoom: 4, minZoom: 4, maxZoom: 16, preferCanvas: true, worldCopyJump: true})
 
 var baseMaps = {
-  Light: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { attribution: "&copy; OpenStreetMap" }).addTo(map),
-  Dark: L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", { attribution: "&copy; Carto Maptiles" }),
+  Light: L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {attribution: "&copy; OpenStreetMap"}).addTo(map),
+  Dark: L.tileLayer("https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png", {attribution: "&copy; Carto Maptiles"}),
   Satellite: L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"),
 }
 
-L.control.locate({ position: "topleft", locateOptions: { flyTo: true, maxZoom: 12 } }).addTo(map)
+var storeIcon = L.icon({iconUrl: "images/marker.svg", iconSize: [34, 34], iconAnchor: [16, 16]})
 
-L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map)
+var supplyIcon = L.icon({iconUrl: "images/supplyChainCenter.svg", iconSize: [34, 34], iconAnchor: [16, 16]})
+
+L.control.layers(baseMaps).addTo(map)
+
+var storeMarkers = L.markerClusterGroup({
+  maxClusterRadius: 60,
+  showCoverageOnHover: true,
+  zoomToBoundsOnClick: true,
+}).addTo(map)
+
+L.control.locate({position: "topleft", locateOptions: {flyTo: true, maxZoom: 12}}).addTo(map)
 
 map.attributionControl.setPrefix('developed by <a href="https://jostasik.com" target="_blank">Joe Stasik</a>')
-
-var overlayMaps = {
-  "Domino's Stores": storeMarkerGroup,
-  "Supply Chain Centers": sccMarker,
-}
